53476bbebc8e7c9e3e02d20e261d382f5b9b03d9
ElishaKwena <elishakwena@gmail.com>
2025-08-20 17:35:06 +0300

    fixed package.json

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true // Disable Next.js image optimization if using Netlify's CDN
   },
-  /* config options here */
-  server: {
-    host: true, // Listen on all network interfaces
-    allowedHosts: [
-      'd01d112a46be.ngrok-free.app', // Your ngrok domain
-      '.ngrok-free.app' // Allow all ngrok domains (recommended)
-    ]
-  }
+
 };
 
 export default nextConfig;
